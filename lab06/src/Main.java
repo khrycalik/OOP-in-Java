a12859987912d67abd9e32408df20da8f9efa03f
khrycalik <86683740+khrycalik@users.noreply.github.com>
2021-12-06 12:18:24 +0100

    Delete lab06/src directory

--- lab06/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        UI ui = new UI(new Scanner(System.in), new Dungeon(5, 5, 2, 10));
-        ui.run();
-    }
-}
